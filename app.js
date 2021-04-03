7b2b9ba029d0678d9e21b33f914a3c9fd6e30da3
mohammad tavakoli <mohammad.mrt7080@gmail.com>
2021-04-03 09:38:52 +0430

    tavakoli js

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-var images = document.getElementsByClassName("load-image");
-images[0].style.display = none;
+function statusFunc(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return Promise.resolve(response);
+    }
+    else {
+        return Promise.reject(new Error(response.statusText));
+    }
+}
+function json(response) {
+    return response.blob();
+}
+window.onload = () => {
+    const image = document.getElementById("image");
+
+    for (let i = 0; i < 3 ; i++) {
+        let clinetWitdh = image.width;
+        let clinetHeight = image.height;
+        fetch(`http://placekitten.com/g/${clinetWitdh}/${clinetHeight}`)
+            .then(statusFunc)
+            .then(json)
+            .then((response) => {
+                const urlObject = URL.createObjectURL(response);
+                image.setAttribute('src', urlObject)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+}
